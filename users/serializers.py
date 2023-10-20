a39739a6e6fa7dcba96934e44d0eb5bafed325dc
Yasu Fadhili <yasufadhili@gmail.com>
2023-10-20 14:54:43 +0300

    added the UserRgistration in apps serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,15 @@
+from django.shortcuts import render
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
 
-from rest_framework import serializers
+from rest_framework import viewsets, permissions, status, generics, serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from knox.views import LoginView as KnoxLoginView
+from knox.auth import TokenAuthentication
+from knox.models import AuthToken
 
 from django_countries.serializers import CountryFieldMixin
 
@@ -37,4 +46,40 @@ class UserRelationshipSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserRelationship
         fields = '__all__'
+
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'phone_number', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+              'style': {'input_type': 'password'}
+            }
+        }
+    
+    def validate_phone_number(self, phone_number):
+        if User.objects.filter(phone_number=phone_number).exists():
+            raise ValidationError("Phone number '%s' already exists")
+        return phone_number
+    
+    def validate_username(self, username):
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("Username '%s already exists" % username)
+        return username
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            phone_number=validated_data['phone_number'],
+            password=validated_data['password']
+        )
+        return user
+
+
+
 
