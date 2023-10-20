b741073e665e64325c3bcdf1ed4dbce4e72a1762
Yasu Fadhili <yasufadhili@gmail.com>
2023-10-20 14:58:34 +0300

    Update users urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 
 from rest_framework.routers import DefaultRouter
 
-from users.views import UserViewSet
+from users.views import UserViewSet, UserRegistrationView
 
 
 router = DefaultRouter()
 router.register(r'', UserViewSet)
 
 urlpatterns = [
-    
+    path("register/", UserRegistrationView.as_view(), name="user-registration"),
 ]
 
 urlpatterns += router.urls
